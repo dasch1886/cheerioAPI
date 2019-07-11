207e16cdb2f5a1004298cdf25c107fd451e5c9ec
kamil.kopacz <kamil.kopacz@comarch.pl>
2019-07-11 14:21:44 +0200

    added handlers

--- src/config/Use.ts
+++ src/config/Use.ts
@@ -2,10 +2,13 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import { requestLogger } from './RequestsLogger';
+import { ingredientRouter } from '../handler/IngredientHandler';
+import { unitRouter } from '../handler/UnitHandler';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(requestLogger);
+app.use('/api', ingredientRouter, unitRouter);
 
 export { app }; 
