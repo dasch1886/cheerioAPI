05ac283242a03c025596f18f491a8c1a4f12d4da
kamil.kopacz <kamil.kopacz@comarch.pl>
2019-07-11 14:16:23 +0200

    corrected connecting with mongodb

--- src/Main.ts
+++ src/Main.ts
@@ -4,12 +4,12 @@ import * as mongoose from 'mongoose';
 
 const PORT = 8080;
 const server = http.createServer(app);
-const url = 'mongodb://localhost/cheerio:27017';
+const url = 'mongodb://localhost:27017/cheerio';
 
 server.listen(process.env.port || PORT, async () => {
     console.info(`Listening on port ${process.env.port || PORT}`);
 
-    mongoose.connect(url, { useNewUrlParser: true }).then(
+    mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true }).then(
         () => console.info(`connected with mongodb ${url}`),
         (err: Error) => console.error(err.message)
     );
