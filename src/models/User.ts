4d1bfc0c017685025270fe5c6618f47854634cb6
kamil.kopacz <k.dasch@interia.pl>
2019-07-12 11:41:00 +0200

    renamed and corrected email RegExp

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const emailRegExp = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'name field is required'],
+        match: [emailRegExp],
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: [true, 'password field is required']
+    },
+
+    nickname: {
+        type: String,
+        required: [true, 'nickname field is required'],
+        unique: true
+    }
+});
+
+const user = mongoose.model('user', userSchema, 'user');
+
+export { user };
