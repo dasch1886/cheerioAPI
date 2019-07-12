a54a1a98462d02afea7101af007df5f3603a0c2f
kamil.kopacz <k.dasch@interia.pl>
2019-07-12 11:44:01 +0200

    added user router

--- src/config/Use.ts
+++ src/config/Use.ts
@@ -4,11 +4,12 @@ import * as bodyParser from 'body-parser';
 import { requestLogger } from './RequestsLogger';
 import { ingredientRouter } from '../handler/IngredientHandler';
 import { unitRouter } from '../handler/UnitHandler';
+import { userRouter } from '../handler/UserHandler';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(requestLogger);
-app.use('/api', ingredientRouter, unitRouter);
+app.use('/api', ingredientRouter, unitRouter, userRouter);
 
 export { app }; 
