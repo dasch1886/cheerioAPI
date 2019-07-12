338a08bd7dcda3c506bb13aa6aeb214d35b4a089
kamil.kopacz <k.dasch@interia.pl>
2019-07-12 14:05:51 +0200

    implemented functions for token

--- src/handler/Auth.ts
+++ src/handler/Auth.ts
@@ -1,4 +1,10 @@
 import * as bcryptjs from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import * as express from 'express';
+import { user } from '../models/User';
+import { notFoundError, internalServerError } from './ErrorHandler';
+
+const tokenPrefix = 'Bearer'
 
 function passwordHash(pass: string): string {
     const salt = bcryptjs.genSaltSync(10);
@@ -9,4 +15,41 @@ function passwordVerify(pass: string, hash: string): boolean {
     return bcryptjs.compareSync(pass, hash);
 }
 
-export { passwordHash, passwordVerify };
+function tokenGenerate(email: string, key: string): string {
+    return jwt.sign(
+        {
+            email: email,
+        },
+        key,
+        {
+            expiresIn: '1min'
+        });
+}
+
+function tokenVerify(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader) {
+        const bearerToken = bearerHeader.split(' ')[1];
+        user.findOne({
+            nickname: req.body.nickname
+        }, (err, doc) => {
+            if (err) {
+                internalServerError(err, res);
+            } else {
+                if (doc) {
+                    jwt.verify(bearerToken, doc.toObject().password, (err) => {
+                        if (err) {
+                            res.status(401).json({ message: 'unauthorized access' });
+                        } else {
+                            next();
+                        }
+                    });
+                } else {
+                    notFoundError(res);
+                }
+            }
+        });
+    }
+}
+
+export { passwordHash, passwordVerify, tokenGenerate, tokenPrefix, tokenVerify };
