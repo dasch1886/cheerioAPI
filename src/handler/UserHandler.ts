d163a8506e1519a2568eb4ebb406bc113dd590b1
kamil.kopacz <k.dasch@interia.pl>
2019-07-15 15:40:53 +0200

    added return type login response

--- src/handler/UserHandler.ts
+++ src/handler/UserHandler.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import { user } from '../models/User';
 import { internalServerError } from './ErrorHandler';
 import { passwordHash, passwordVerify, tokenGenerate, tokenPrefix } from './Auth';
+import { loginResponse } from '../models/LoginResponse';
 
 const userRouter = express.Router();
 
@@ -9,6 +10,7 @@ userRouter.post('/user', async (req: express.Request, res: express.Response) => 
     const emailValue = req.body.email;
     const nicknameValue = req.body.nickname;
     const passwordValue = req.body.password;
+
     await user.create({
         email: emailValue,
         password: passwordHash(passwordValue),
@@ -26,6 +28,7 @@ userRouter.post('/user', async (req: express.Request, res: express.Response) => 
 userRouter.get('/user', async (req: express.Request, res: express.Response) => {
     const loginValue = req.body.login;
     const passwordValue = req.body.password;
+
     await user.findOne({
         $or: [
             {
@@ -43,12 +46,12 @@ userRouter.get('/user', async (req: express.Request, res: express.Response) => {
                 if (doc) {
                     const userValue = doc.toObject();
                     if (passwordVerify(passwordValue, userValue.password)) {
-                        res.json(
-                            {
-                                nickname: userValue.nickname,
-                                token_type: tokenPrefix,
-                                access_token: tokenGenerate(userValue.email, userValue.password)
-                            });
+                        const response: loginResponse = {
+                            nickname: userValue.nickname,
+                            token_type: tokenPrefix,
+                            access_token: tokenGenerate(userValue.email, userValue.password)
+                        }
+                        res.json(response);
                     } else {
                         res.status(400).json({ message: 'invalid password' });
                     }
