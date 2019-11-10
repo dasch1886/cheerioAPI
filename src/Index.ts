14901076db701f0f8bb515a2ee1f3fa59d2fab85
dasch1886 <k.dasch@interia.pl>
2019-11-10 16:33:53 +0100

    set connection with remote db

--- src/Index.ts
+++ src/Index.ts
@@ -4,7 +4,9 @@ import * as mongoose from 'mongoose';
 
 const PORT = 8080;
 export const server = http.createServer(app);
-const url = 'mongodb://localhost:27017/cheerio';
+const username = 'test';
+const password = 'test';
+const url = `mongodb+srv://${username}:${password}@cheeriov2-daxgk.mongodb.net/test?retryWrites=true&w=majority`;
 
 server.listen(process.env.port || PORT, async () => {
     console.info(`Listening on port ${process.env.port || PORT}`);
