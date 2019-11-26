261408071787d6ba0e59a14aa2385d86d710f63f
dasch1886 <k.dasch@interia.pl>
2019-11-26 20:47:13 +0100

    init scrapper

---
+++ src/scrapper/Index.ts
@@ -0,0 +1,61 @@
+import * as cheerio from 'cheerio';
+import * as pr from 'request-promise';
+import { getRecipes } from './SplitData';
+import { recipe } from '../models/Recipe';
+
+export const host = 'http://www.przepisy.pl';
+export const mainUri = ['/przepisy/influencerzy', '/przepisy/dania-i-przekaski/dania-glowne'];
+
+export const options: pr.RequestPromiseOptions = {
+    method: 'GET',
+    host: '',
+    headers: {
+        'User-Agent': 'cheerioAPI',
+    }
+};
+
+export async function Initscrapping() {
+    for(const uri of mainUri) {
+        options.host = host + uri;
+        await pr(options.host, options).then((response) => {
+            const $ = cheerio.load(response);
+            getRecipes($).then(res => {
+                const recipes = getUnique(res, 'name');
+                setRecipesToDB(recipes);
+            });
+        });
+    }
+    
+}
+
+async function setRecipesToDB(recipes: Array<any>) {
+    recipes.forEach(async (el) => {
+        await recipe.create({
+            name: el.name,
+            author: el.author,
+            desc: el.desc,
+            ingredients: el.ingredients,
+            imgPath: el.imgPath
+        }).then(doc => {
+            console.log('Scrapping done');
+        }).catch(err => {
+            console.info('Error during adding recipe to db');
+        });
+    })
+
+}
+
+function getUnique(arr, comp) {
+
+    const unique = arr
+        //store the comparison  values in array
+        .map(e => e[comp])
+
+        // store the keys of the unique objects
+        .map((e, i, final) => final.indexOf(e) === i && i)
+
+        // eliminate the dead keys & store unique objects
+        .filter(e => arr[e]).map(e => arr[e]);
+
+    return unique;
+}
---
+++ src/scrapper/SplitData.ts
@@ -0,0 +1,108 @@
+import * as cheerio from 'cheerio';
+import { options, host } from './Index';
+import * as pr from 'request-promise';
+import * as fs from 'fs';
+
+const getRecipeDetailsUrl = /.*www.przepisy\.pl/;
+const getImgName = /[^\/]*\.(jpg|png|jpeg)/;
+const imagesPath = 'public/';
+
+export async function getRecipes(htmlDoc: CheerioStatic): Promise<Array<any>> {
+    const $ = htmlDoc;
+
+    const tmp: Array<any> = $('.recipe-wrapper').map((i, el) => {
+        return el
+    }).toArray();
+
+    const recipes = tmp.map(async (el) => {
+        const data = {
+            name: '',
+            author: '',
+            desc: '',
+            ingredients: [],
+            imgPath: ''
+        };
+
+        data.author = getAuthor($, el);
+        data.name = getRecipeTitle($, el);
+
+        const link = getRecipeDetailsLinkHref($, el);
+        const settings: pr.RequestPromiseOptions = options;
+        settings.host = host + link.replace(getRecipeDetailsUrl, '');
+
+        await getRecipeDetailsHtml(settings).then(async (res) => {
+            const recipeDetails = cheerio.load(res);
+
+            data.ingredients = getRecipeIngredients(recipeDetails);
+            data.desc = getRecipeDesc(recipeDetails);
+
+            const imgUri = getImgUri(recipeDetails);
+            const imgName = imgUri.match(getImgName)[0];
+
+            await getImage(imgUri, settings).then((imgFile) => {
+
+                const imgPath: string = `${imagesPath}_${imgName}`;
+                fs.writeFileSync(imgPath, imgFile);
+
+                data.imgPath = imgPath;
+
+            }).catch(err => console.warn(`Problem with get image`));
+
+        }).catch(err => console.warn(`Problem with recipe details site`));
+
+        return data;
+    });
+
+    return Promise.all(recipes);
+};
+
+function getImage(uri: string, options: pr.RequestPromiseOptions): pr.RequestPromise {
+    const settings = options;
+    settings.host = uri.replace(getRecipeDetailsUrl, '');
+    settings.encoding = null;
+    return pr(uri, settings);
+}
+
+function resetRecipe(recipe) {
+    recipe.name = '';
+    recipe.ingredients = [];
+    recipe.author = '',
+        recipe.desc = '';
+    recipe.img = '';
+};
+
+function getAuthor($: CheerioStatic, el: CheerioElement): string {
+    return $(el).find('.author-name').text();
+};
+
+function getRecipeTitle($: CheerioStatic, el: CheerioElement): string {
+    return $(el).find('a.recipe-box__title').text();
+}
+
+function getRecipeDetailsLinkHref($: CheerioStatic, el: CheerioElement): string {
+    return $(el).find('.recipe-box > a').attr('href');
+}
+
+function getRecipeIngredients($: CheerioStatic): Array<any> {
+    return $('.ingredient-ul .row-ingredient').map((i, el) => {
+        return {
+            name: $(el).find('.ingredient-name').text(),
+            amount: $(el).find('.quantity').text().split(' ')[0],
+            unitName: $(el).find('.quantity').text().split(' ')[1],
+        }
+    }).toArray();
+}
+
+function getRecipeDesc($: CheerioStatic): string {
+    return $('.recipe-container-steps li').map((i, el) => {
+        return $(el).find('.step-responsive-text').text();
+    }).toArray().toString();
+}
+
+function getImgUri($: CheerioStatic): string {
+    return $('.image-container').find('img').attr('data-src');
+}
+
+function getRecipeDetailsHtml(options: pr.RequestPromiseOptions): pr.RequestPromise {
+    return pr(options.host, options);
+}
