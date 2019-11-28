3472dc5154c1217ed73f08ecf5399845509c8eb7
dasch1886 <k.dasch@interia.pl>
2019-11-28 22:18:31 +0100

    added image router

--- src/config/Use.ts
+++ src/config/Use.ts
@@ -6,10 +6,11 @@ import { ingredientRouter } from '../handler/IngredientHandler';
 import { unitRouter } from '../handler/UnitHandler';
 import { userRouter } from '../handler/UserHandler';
 import { recipeRouter } from '../handler/RecipeHandler';
+import { imgRouter } from '../handler/ImgHandler';
 
 export const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(requestLogger);
-app.use('/api', ingredientRouter, unitRouter, userRouter, recipeRouter);
+app.use('/api', ingredientRouter, unitRouter, userRouter, recipeRouter, imgRouter);
