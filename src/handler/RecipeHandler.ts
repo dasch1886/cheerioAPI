390f9d0f9b4c487cd446fe5e6c25ff907bac7956
kamil.kopacz <k.dasch@interia.pl>
2019-12-27 16:19:29 +0100

    refactor, commenting recipes and get comments added

--- src/handler/RecipeHandler.ts
+++ src/handler/RecipeHandler.ts
@@ -43,16 +43,58 @@ recipeRouter.post('/recipe', tokenVerify, async (req: express.Request, res: expr
     next();
 });
 
-recipeRouter.get('/recipe', tokenVerify, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const text = req.body.text || '';
+recipeRouter.get('/recipe', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const id = req.query.id;
 
-    await recipe.find({
-        $text: {
-            $search: text
-        }
-    }).then(
+    await recipe.findById(id).then(
         doc => {
-            if (doc.length !== 0) res.json(doc);
+            if (doc) res.json(doc);
+            else notFoundError(res);
+        },
+        err => {
+            internalServerError(err, res);
+        }
+    );
+    next();
+});
+
+recipeRouter.post('/recipe/comment', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const id = req.body.id;
+    const author = req.body.author;
+    const value = req.body.value;
+
+    await recipe.update(
+        {
+            _id: id
+        },
+        {
+            $push: {
+                comments: {
+                    author: author,
+                    value: value,
+                }
+            }
+        }).then(
+            doc => {
+                if (doc) res.json({
+                    success: true,
+                    data: doc
+                });
+                else notFoundError(res);
+            },
+            err => {
+                internalServerError(err, res);
+            }
+        );
+    next();
+});
+
+recipeRouter.get('/recipe/comment', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const id = req.query.id;
+
+    await recipe.findById(id).then(
+        doc => {
+            if (doc) res.json(doc.get('comments'));
             else notFoundError(res);
         },
         err => {
